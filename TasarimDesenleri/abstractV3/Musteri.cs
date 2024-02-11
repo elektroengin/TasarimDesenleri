f45ef88883c4399158174095beaa66cb189beb04
Engin EFE <elektroengin@gmail.com>
2024-02-11 17:27:54 +0300

    designpatterns sample

---
+++ TasarimDesenleri/abstractV3/Musteri.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace abstractV3
+{
+    class Musteri
+    {
+        static void Main(string[] args)
+        {
+            //Bmw marka araçlarının üretildiği fabrika olulturduk.
+            ArabaFabrika bmw = new BmwFabrika();
+
+            //Bmw marka araçlarının üretildiği fabrika olulturduk.
+            ArabaFabrika audi = new AudiFabrika();
+
+            foreach (var item in bmw.ArabaListesi)
+            {
+                Console.WriteLine(item.Marka + "-" + item.Model + "-" + item.BeygirGucu);
+            }
+
+            foreach (var item in audi.ArabaListesi)
+            {
+                Console.WriteLine(item.Marka + "-" + item.Model + "-" + item.BeygirGucu);
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
